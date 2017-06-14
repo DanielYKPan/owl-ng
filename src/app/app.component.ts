0bb637f9c1324b876bb23d17944d8592ac6b8aa6
Yaokun Pan <myron.yk.pan@gmail.com>
2017-06-14 23:44:00 +1000

    Initial Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+/**
+ * app.component
+ */
+
+import { Component, OnInit } from "@angular/core";
+
+import '../sass/main.scss';
+
+@Component({
+    selector: 'yk-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent implements OnInit {
+
+    constructor() {
+    }
+
+    public ngOnInit(): void {
+    }
+}
